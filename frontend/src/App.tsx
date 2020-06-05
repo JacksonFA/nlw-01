f9d2cffd8cda46c24a44bb3c970750e3394b7262
Softilux7 <66313209+Softilux7@users.noreply.github.com>
2020-06-05 10:45:19 -0300

    App

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//Estilos
+import './App.css';
+
+//Componentes
+import Routes from './routes';
+
+function App() {
+  return (
+      <Routes />
+  );
+}
+
+export default App;
